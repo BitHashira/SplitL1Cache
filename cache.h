2107207c798e5f30479040b950aaf615f8d8cf7c
BitHashira <ameyk@pdx.edu>
2025-02-28 17:16:16 -0800

    1. Separated the DS in cache.h 2. Implemented Case 0 (READ)
    new file:   a.out
    new file:   cache.h
    modified:   splitL1Cache.c
    new file:   trace_test.txt

---
+++ cache.h
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#define L1_CACHE_SETS 16384
+#define L1_CACHE_ADDR 32
+#define L1_ICACHE_WAYS 4
+#define L1_DCACHE_WAYS 8
+#define CACHE_LINE_SIZE 64
+
+#define L1_CACHE_INDEX __builtin_ctz(L1_CACHE_SETS)
+#define L1_CACHE_OFFSET __builtin_ctz(CACHE_LINE_SIZE)
+#define L1_CACHE_TAG (L1_CACHE_ADDR - L1_CACHE_INDEX - L1_CACHE_OFFSET)
+
+// MESI protocol states
+typedef enum
+{
+    INVALID,
+    EXCLUSIVE,
+    SHARED,
+    MODIFIED
+} MESI_State;
+
+const char *MESI_State_strings[] = {
+    "INVALID",
+    "EXCLUSIVE",
+    "SHARED",
+    "MODIFIED"};
+
+// Structure representing a cache line
+typedef struct
+{
+    bool valid; // Valid bit
+    // bool tag[L1_CACHE_TAG]; // Tag for address mapping
+    uint32_t tag;        // Tag for address mapping
+    MESI_State state;    // MESI state
+    uint8_t lru_counter; // Counter for LRU replacement
+} CacheLine;
+
+// Structure representing an instruction cache set
+typedef struct
+{
+    CacheLine lines[L1_ICACHE_WAYS];
+} ICacheSet;
+
+// Structure representing a data cache set
+typedef struct
+{
+    CacheLine lines[L1_DCACHE_WAYS];
+} DCacheSet;
+
+//
+// int main(void)
+// {
+//     printf("L1_CACHE_OFFSET = %d\n", L1_CACHE_OFFSET); // 6
+//     printf("L1_CACHE_INDEX  = %d\n", L1_CACHE_INDEX);  // 14
+//     printf("L1_CACHE_TAG    = %d\n", L1_CACHE_TAG);    // 12
+//     return 0;
+// }
+//
--- splitL1Cache.c
+++ splitL1Cache.c
@@ -1,53 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <stdbool.h>
-
-#define L1_ICACHE_SETS 16384
-#define L1_DCACHE_SETS 16384
-#define L1_ICACHE_WAYS 4
-#define L1_DCACHE_WAYS 8
-#define CACHE_LINE_SIZE 64
-
-// MESI protocol states
-typedef enum { INVALID, EXCLUSIVE, SHARED, MODIFIED } MESI_State;
-
-// Structure representing a cache line
-typedef struct {
-    uint32_t tag;       // Tag for address mapping
-    MESI_State state;   // MESI state
-    bool valid;         // Valid bit
-    int lru_counter;    // Counter for LRU replacement
-} CacheLine;
-
-// Structure representing an instruction cache set
-typedef struct {
-    CacheLine lines[L1_ICACHE_WAYS];
-} ICacheSet;
-
-// Structure representing a data cache set
-typedef struct {
-    CacheLine lines[L1_DCACHE_WAYS];
-} DCacheSet;
+#include "cache.h"
 
 // L1 instruction and data caches
-ICacheSet icache[L1_ICACHE_SETS];
-DCacheSet dcache[L1_DCACHE_SETS];
+ICacheSet icache[L1_CACHE_SETS];
+DCacheSet dcache[L1_CACHE_SETS];
 
 // Cache statistics
 uint32_t cache_reads = 0, cache_writes = 0, cache_hits = 0, cache_misses = 0;
 
 // Function to initialize caches
-void initialize_cache() {
-    for (int i = 0; i < L1_ICACHE_SETS; i++) {
-        for (int j = 0; j < L1_ICACHE_WAYS; j++) {
+void initialize_cache()
+{
+    for (int i = 0; i < L1_CACHE_SETS; i++)
+    {
+        for (int j = 0; j < L1_ICACHE_WAYS; j++)
+        {
             icache[i].lines[j].valid = false;
             icache[i].lines[j].state = INVALID;
             icache[i].lines[j].lru_counter = j;
         }
     }
-    for (int i = 0; i < L1_DCACHE_SETS; i++) {
-        for (int j = 0; j < L1_DCACHE_WAYS; j++) {
+    for (int i = 0; i < L1_CACHE_SETS; i++)
+    {
+        for (int j = 0; j < L1_DCACHE_WAYS; j++)
+        {
             dcache[i].lines[j].valid = false;
             dcache[i].lines[j].state = INVALID;
             dcache[i].lines[j].lru_counter = j;
@@ -55,19 +30,153 @@ void initialize_cache() {
     }
 }
 
-// Function to update the LRU counters upon access
-void update_lru(CacheLine *lines, int ways, int accessed_index) {
-    int accessed_lru = lines[accessed_index].lru_counter;
-    for (int i = 0; i < ways; i++) {
-        if (lines[i].lru_counter < accessed_lru) {
-            lines[i].lru_counter++;
+void print_cache_index(bool whichCache, uint32_t index)
+{
+    if (!whichCache)
+    {
+        printf("D-Cache Data - \n");
+        DCacheSet tmp = dcache[index];
+        for (int i = 0; i < L1_DCACHE_WAYS; i++)
+        {
+            printf("Way: %d\n", i);
+            printf("Valid: %b\t", tmp.lines[i].valid);
+            printf("Tag: %03x\t", tmp.lines[i].tag);
+            printf("lru_counter: %d\t", tmp.lines[i].lru_counter);
+            printf("MESI State: %s\t", MESI_State_strings[tmp.lines[i].state]);
+            printf("\n");
         }
     }
-    lines[accessed_index].lru_counter = 0;
+    else
+    {
+        printf("I-Cache Data - \n");
+        ICacheSet tmp = icache[index];
+        for (int i = 0; i < L1_ICACHE_WAYS; i++)
+        {
+            printf("Way: %d\n", i);
+            printf("Valid: %b\t", tmp.lines[i].valid);
+            printf("Tag: %03x\t", tmp.lines[i].tag);
+            printf("lru_counter: %d\t", tmp.lines[i].lru_counter);
+            printf("MESI State: %s\t", MESI_State_strings[tmp.lines[i].state]);
+        }
+    }
 }
 
-// Function to print cache statistics
-void print_statistics() {
+void update_lru(bool is_i_or_d, int way, uint32_t index, uint32_t tag)
+{
+    if (~is_i_or_d)
+    {
+        int accessed_lru_cnt = dcache[index].lines[way].lru_counter;
+        for (int i = 0; i < L1_DCACHE_WAYS; i++)
+        {
+            if (dcache[index].lines[i].lru_counter > accessed_lru_cnt)
+            {
+                dcache[index].lines[i].lru_counter--;
+            }
+        }
+
+        dcache[index].lines[way].lru_counter = L1_DCACHE_WAYS;
+    }
+}
+
+bool read_cache(bool is_i_or_d, uint32_t index, uint32_t tag)
+{
+    bool isHit = false;
+    if (~is_i_or_d)
+    {
+        // This is D-Cache
+        for (int i = 0; i < L1_DCACHE_WAYS; i++)
+        {
+            // check if valid bit is 1
+            if (dcache[index].lines[i].valid)
+            {
+                // Valid bit is set, now compare tags
+                if (dcache[index].lines[i].tag == tag)
+                {
+                    // Tag bit also matches, this means we have a Hit
+                    isHit = true;
+                    update_lru(is_i_or_d, i, index, tag);
+                    break;
+                }
+            }
+        }
+        if (!isHit)
+        {
+            // Handle the miss
+            for (int i = 0; i < L1_DCACHE_WAYS; i++)
+            {
+                // Find the way which has lru_counter set to 0 i.e. it is LRU
+                if (dcache[index].lines[i].lru_counter == 0)
+                {
+                    dcache[index].lines[i].valid = true;
+                    dcache[index].lines[i].tag = tag;
+                    dcache[index].lines[i].state = EXCLUSIVE;
+                    update_lru(is_i_or_d, i, index, tag);
+                    break;
+                }
+            }
+        }
+        return isHit;
+    }
+}
+
+// Function to access the cache (read/write operations)
+void access_cache(uint32_t address, int operation)
+{
+    printf("\nYou have given me case: %d\n", operation);
+
+    bool is_write = (operation == 1);
+    bool is_i_or_d = (operation == 2);
+
+    uint32_t index = (address / CACHE_LINE_SIZE) % L1_CACHE_SETS;
+    uint32_t tag = address / (CACHE_LINE_SIZE * L1_CACHE_SETS);
+
+    printf("Given Addr: %x\n", address);
+    printf("Index: %x\n", index);
+    printf("Tag: %x\n", tag);
+
+    switch (operation)
+    {
+    case 0:
+        cache_reads++;
+        if (read_cache(is_i_or_d, index, tag))
+        {
+            cache_hits++;
+        }
+        else
+        {
+            cache_misses++;
+        }
+        break;
+
+    default:
+        break;
+    }
+
+    print_cache_index(is_i_or_d, index);
+}
+
+// Function to process trace file and simulate cache behavior
+void process_trace_file(const char *filename)
+{
+    FILE *file = fopen(filename, "r");
+    if (!file)
+    {
+        perror("Error opening trace file");
+        return;
+    }
+
+    int operation;
+    uint32_t address;
+    while (fscanf(file, "%d %x", &operation, &address) == 2)
+    {
+        access_cache(address, operation);
+    }
+    fclose(file);
+}
+
+void print_stats()
+{
+    printf("\nFinal Stats - \n");
     printf("Cache Reads: %u\n", cache_reads);
     printf("Cache Writes: %u\n", cache_writes);
     printf("Cache Hits: %u\n", cache_hits);
@@ -75,120 +184,16 @@ void print_statistics() {
     printf("Cache Hit Ratio: %.2f%%\n", (cache_hits * 100.0) / (cache_hits + cache_misses));
 }
 
-
-// Function to access the cache (read/write operations)
-void access_cache(uint32_t address, int operation) {
-    printf("You have given me case: %d\n", operation);
-
-    bool is_write = (operation == 1);
-    bool is_instruction = (operation == 2);
-    
-    uint32_t index = (address / CACHE_LINE_SIZE) % (is_instruction ? L1_ICACHE_SETS : L1_DCACHE_SETS);
-    uint32_t tag = address / (CACHE_LINE_SIZE * (is_instruction ? L1_ICACHE_SETS : L1_DCACHE_SETS));
-    
-    CacheLine *lines = is_instruction ? icache[index].lines : dcache[index].lines;
-    int ways = is_instruction ? L1_ICACHE_WAYS : L1_DCACHE_WAYS;
-    
-    if (operation != 9) {
-      cache_reads += !is_write;
-      cache_writes += is_write;
-    }
-
-    for (int i = 0; i < ways; i++) {
-        if (lines[i].valid && lines[i].tag == tag) {
-            if (operation != 9) {
-              cache_hits++;
-            }
-            if (is_write) {
-                lines[i].state = MODIFIED;
-            }
-            update_lru(lines, ways, i);
-            if (operation != 9 || operation != 3 || operation != 4 || operation != 8) {
-                return;
-                // break;
-            }
-        }
-    }
-    
-    if (operation != 9) {
-      cache_misses++;
-    } 
-    // Finding the LRU cache line for replacement
-    int lru_index = 0;
-    for (int i = 0; i < ways; i++) {
-        if (!lines[i].valid) {
-            lru_index = i;
-            break;
-        }
-        if (lines[i].lru_counter == ways - 1) {
-            lru_index = i;
-        }
-    }
-    
-    // Write back if the line is modified before eviction
-    if (lines[lru_index].valid && lines[lru_index].state == MODIFIED) {
-        printf("Write to L2 %08X\n", address);
-    }
-    
-    // Handle special operations
-    switch (operation) {
-        case 0: case 1: case 2:
-            printf("Read from L2 %08X\n", address);
-            lines[lru_index].tag = tag;
-            lines[lru_index].valid = true;
-            lines[lru_index].state = is_write ? MODIFIED : SHARED;
-            update_lru(lines, ways, lru_index);
-            break;
-        case 3:
-            for (int i = 0; i < L1_DCACHE_WAYS; i++) {
-                if (dcache[index].lines[i].valid && dcache[index].lines[i].tag == tag) {
-                    dcache[index].lines[i].valid = false;
-                    printf("Invalidate command from L2 %08X\n", address);
-                    fflush(stdout);
-                    return;
-                }
-            }
-            break;
-        case 4:
-            for (int i = 0; i < L1_DCACHE_WAYS; i++) {
-                if (dcache[index].lines[i].valid && dcache[index].lines[i].tag == tag) {
-                    printf("Data request from L2 %08X - Data Found\n", address);
-                    fflush(stdout);
-                    return;
-                }
-            }
-            printf("Data request from L2 %08X - Data Not Found\n", address);
-            fflush(stdout);
-            break;
-        // case 3: printf("Invalidate command from L2 %08X\n", address); break;
-        // case 4: printf("Data request from L2 %08X\n", address); fflush(stdout); break;
-        case 8: initialize_cache(); printf("Cache cleared and reset\n"); return;
-        case 9: print_statistics(); fflush(stdout); break;
-    }
-}
-// Function to process trace file and simulate cache behavior
-void process_trace_file(const char *filename) {
-    FILE *file = fopen(filename, "r");
-    if (!file) {
-        perror("Error opening trace file");
-        return;
-    }
-    
-    int operation;
-    uint32_t address;
-    while (fscanf(file, "%d %x", &operation, &address) == 2) {
-        access_cache(address, operation);
-    }
-    fclose(file);
-}
-
 // Main function to initialize and run the simulation
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
         fprintf(stderr, "Usage: %s <trace_file>\n", argv[0]);
         return 1;
     }
     initialize_cache();
     process_trace_file(argv[1]);
+    print_stats();
     return 0;
 }
